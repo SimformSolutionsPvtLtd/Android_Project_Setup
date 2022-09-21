8d68a022c91a23282b016d8a2d83c23ce45b73d0
Jitendra <jitendra.simformsolutions@gmail.com>
2022-09-21 15:24:33 +0530

    - All lib version updated - AGP,build tools plugin updated - Hiltviewmodel removed in new version

--- {{ cookiecutter.repo_name }}/app/build.gradle.kts
+++ {{ cookiecutter.repo_name }}/app/build.gradle.kts
@@ -114,7 +114,7 @@ dependencies {
     {% endif %}
     // Hilt
     implementation(Libs.HILT)
-    implementation(Libs.HILT_VIEWMODEL)
+   // implementation(Libs.HILT_VIEWMODEL)
     kapt(Libs.HILT_DAGGER_COMPILER)
     kapt(Libs.HILT_COMPILER)
 
--- {{ cookiecutter.repo_name }}/app/src/main/java/{{ cookiecutter.package_dir }}/viewmodel/SplashViewModel.kt
+++ {{ cookiecutter.repo_name }}/app/src/main/java/{{ cookiecutter.package_dir }}/viewmodel/SplashViewModel.kt
@@ -15,7 +15,7 @@
 */
 package {{ cookiecutter.package_name }}.viewmodel
 
-import androidx.hilt.lifecycle.ViewModelInject
+
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
