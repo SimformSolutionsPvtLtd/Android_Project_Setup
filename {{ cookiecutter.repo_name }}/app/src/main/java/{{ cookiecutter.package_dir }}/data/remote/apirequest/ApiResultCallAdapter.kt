e8c88cf3b359e33e68c77e38f6e66b0498a8e4b2
Vraj Shah <vraj.s@simformsolutions.com>
2023-02-03 15:43:34 +0530

    UNT-T12274 Added custom retrofit api request call adapter factory

---
+++ {{ cookiecutter.repo_name }}/app/src/main/java/{{ cookiecutter.package_dir }}/data/remote/apirequest/ApiResultCallAdapter.kt
@@ -0,0 +1,17 @@
+package {{ cookiecutter.package_name }}.data.remote.apirequest
+
+import retrofit2.Call
+import retrofit2.CallAdapter
+import java.lang.reflect.Type
+
+/**
+ * Custom [CallAdapter] to adapt call with [responseType] into [ApiResult]
+ */
+class ApiResultCallAdapter(
+    private val responseType: Type
+) : CallAdapter<Type, Call<ApiResult<Type>>> {
+
+    override fun responseType(): Type = responseType
+
+    override fun adapt(call: Call<Type>): Call<ApiResult<Type>> = ApiResultCall(call)
+}
--- {{ cookiecutter.repo_name }}/app/src/main/java/{{ cookiecutter.package_dir }}/ui/sample/SampleViewModel.kt
+++ {{ cookiecutter.repo_name }}/app/src/main/java/{{ cookiecutter.package_dir }}/ui/sample/SampleViewModel.kt
@@ -1,27 +1,32 @@
 package {{ cookiecutter.package_name }}.ui.sample
 
-
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import {{ cookiecutter.package_name }}.ui.base.BaseViewModel
-import {{ cookiecutter.package_name }}.data.repository.UserRepository
 import {{ cookiecutter.package_name }}.data.remote.response.User
+import {{ cookiecutter.package_name }}.data.repository.UserRepository
+import {{ cookiecutter.package_name }}.di.IoDispatcher
+import {{ cookiecutter.package_name }}.di.MainDispatcher
+import {{ cookiecutter.package_name }}.ui.base.BaseViewModel
+import {{ cookiecutter.package_name }}.utils.extension.onError
+import {{ cookiecutter.package_name }}.utils.extension.onException
+import {{ cookiecutter.package_name }}.utils.extension.onSuccess
 import {{ cookiecutter.package_name }}.utils.result.Event
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import timber.log.Timber
 import javax.inject.Inject
-import dagger.hilt.android.lifecycle.HiltViewModel
 
 /**
  * ViewModel for [{{ cookiecutter.package_name }}.ui.sample.SampleActivity]
  */
 @HiltViewModel
 class SampleViewModel @Inject constructor(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    @MainDispatcher private val mainDispatcher: CoroutineDispatcher
 ) : BaseViewModel() {
 
     private val _onNewUserList = MutableLiveData<Event<List<User>>>()
@@ -50,27 +55,35 @@ class SampleViewModel @Inject constructor(
             _showShimmer.value = true
         }
 
-        viewModelScope.launch(Dispatchers.IO) {
-            runCatching {
-                userRepository.loadUsers(currentPage)
-            }.onSuccess {
-                withContext(Dispatchers.Main) {
-                    _isLoadingPage.value = false
-                    _onNewUserList.value = Event(it)
-                    if (currentPage == 1) {
-                        _showShimmer.value = false
+        viewModelScope.launch(ioDispatcher) {
+            userRepository
+                .loadUsers(currentPage)
+                .onSuccess {
+                    withContext(mainDispatcher) {
+                        _isLoadingPage.value = false
+                        _onNewUserList.value = Event(it.results)
+                        if (currentPage == 1) {
+                            _showShimmer.value = false
+                        }
                     }
+                }.onError { code, message ->
+                    Timber.e(code.toString() + message)
+                    withContext(mainDispatcher) { handleApiFailure() }
+                }.onException {
+                    Timber.e(it.message)
+                    withContext(mainDispatcher) { handleApiFailure() }
                 }
-            }.onFailure {
-                Timber.e(it)
-                withContext(Dispatchers.Main) {
-                    _isLoadingPage.value = false
-                    if (currentPage == 1) {
-                        _showShimmer.value = false
-                    }
-                    currentPage -= 1 // Revert increased count
-                }
-            }
         }
+    }
+
+    /**
+     * Handle the failure occuring in API call
+     */
+    private fun handleApiFailure() {
+        _isLoadingPage.value = false
+        if (currentPage == 1) {
+            _showShimmer.value = false
+        }
+        currentPage -= 1 // Revert increased count
     }
 }
