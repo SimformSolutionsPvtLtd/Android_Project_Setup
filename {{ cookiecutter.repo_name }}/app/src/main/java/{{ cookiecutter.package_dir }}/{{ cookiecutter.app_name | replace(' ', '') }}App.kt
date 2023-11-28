05a5bf7b03b8f2130ab78b3ebe0e4b5fe38a7c28
Jaydeep Pipaliya <jaydeep@simformsolutions.com>
2023-11-28 12:56:40 +0530

    Update Internet Connectivity Code & Update Libs

--- {{ cookiecutter.repo_name }}/app/src/main/java/{{ cookiecutter.package_dir }}/{{ cookiecutter.app_name | replace(' ', '') }}App.kt
+++ {{ cookiecutter.repo_name }}/app/src/main/java/{{ cookiecutter.package_dir }}/{{ cookiecutter.app_name | replace(' ', '') }}App.kt
@@ -58,30 +58,38 @@ class {{ cookiecutter.app_name | replace(' ', '') }}App : Application() {
             getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
         val networkRequest = NetworkRequest.Builder()
             .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
             .build()
-        connectivityManager.registerNetworkCallback(
-            networkRequest,
-            object : ConnectivityManager.NetworkCallback() {
-                override fun onAvailable(network: Network) {
-                    val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
-                    val hasInternet =
-                        networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-                            ?: false
-                    if (hasInternet) {
-                        validNetworks.add(network)
-                        checkValidNetworks()
-                    }
-                }
-
-                override fun onLost(network: Network) {
-                    validNetworks.remove(network)
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (!hasInternet(capabilities)) {
+            checkValidNetworks()
+        }
+        connectivityManager.registerNetworkCallback(networkRequest, object :
+            ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+                if (hasInternet(networkCapabilities)) {
+                    validNetworks.add(network)
                     checkValidNetworks()
                 }
             }
-        )
+
+            override fun onLost(network: Network) {
+                validNetworks.remove(network)
+                checkValidNetworks()
+            }
+        })
     }
 
     private fun checkValidNetworks() {
         NetworkUtil.isNetworkConnected = validNetworks.isNotEmpty()
     }
+
+    private fun hasInternet(networkCapabilities: NetworkCapabilities?): Boolean =
+        networkCapabilities != null &&
+                networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
 }
