657853332d74888f42bcc941cac5f6c201c591a9
Jaydeep Pipaliya <jaydeep@simformsolutions.com>
2023-12-19 17:50:22 +0530

    Migrate to Gradle Version Catalog

--- {{ cookiecutter.repo_name }}/buildSrc/src/main/kotlin/App.kt
+++ {{ cookiecutter.repo_name }}/buildSrc/src/main/kotlin/App.kt
@@ -1,11 +1,6 @@
 object App {
     const val ID = "{{ cookiecutter.package_name }}"
 
-    object Version {
-        const val CODE = 1
-        const val NAME = "1.0"
-    }
-
     object Dimension {
         const val DEFAULT = "default"
     }
--- {{ cookiecutter.repo_name }}/buildSrc/src/main/kotlin/Urls.kt
+++
@@ -1,4 +0,0 @@
-object Urls {
-    const val JITPACK = "https://jitpack.io"
-    const val KOTLINX = "https://kotlin.bintray.com/kotlinx/"
-}
